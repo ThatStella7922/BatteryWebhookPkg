7ce53bc0f77d95ecccc29eb565742fbaf769d059
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2024-02-26 22:55:13 -0500

    dedicated repo for Pkg, start working on date stuff

---
+++ Sources/BatteryWebhookLib/BatteryWebhookLib.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class BatteryWebhookUtilities {
+    public static func getVersion() -> Double {
+        return BatteryWebhookVersion.version
+    }
+}
